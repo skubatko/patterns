c7fed1fb79f2521b782eeb97b71748dccca4482a
Sergey Kubatko <skubatko@gmail.com>
2020-01-07 16:12:21 +0300

    baeldung builder

---
+++ builder/src/main/java/ru/skubatko/dev/patterns/builder/baeldung/BuilderPatternDriver.java
@@ -0,0 +1,17 @@
+package ru.skubatko.dev.patterns.builder.baeldung;
+
+public class BuilderPatternDriver {
+
+    public static void main(String[] args) {
+        BankAccount newAccount = new BankAccount
+          .BankAccountBuilder("Jon", "22738022275")
+          .withEmail("jon@example.com")
+          .wantNewsletter(true)
+          .build();
+
+        System.out.println("Name: " + newAccount.getName());
+        System.out.println("AccountNumber:" + newAccount.getAccountNumber());
+        System.out.println("Email: " + newAccount.getEmail());
+        System.out.println("Want News letter?: " + newAccount.isNewsletter());
+    }
+}
