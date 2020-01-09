1a365003d886bb43e6fc9dcbfd656cf40165e3b4
Sergey Kubatko <skubatko@gmail.com>
2020-01-10 02:08:29 +0300

    baeldung command

--- command/src/main/java/ru/skubatko/dev/patterns/baeldung/client/TextFileApplication.java
+++ command/src/main/java/ru/skubatko/dev/patterns/baeldung/client/TextFileApplication.java
@@ -9,11 +9,17 @@ import ru.skubatko.dev.patterns.baeldung.receiver.TextFile;
 public class TextFileApplication {
     
     public static void main(String[] args) {
-        
-        TextFileOperation openTextFileOperation = new OpenTextFileOperation(new TextFile("file1.txt"));
-        TextFileOperation saveTextFileOperation = new SaveTextFileOperation(new TextFile("file2.txt"));
-        TextFileOperationExecutor textFileOperationExecutor = new TextFileOperationExecutor();
-        System.out.println(textFileOperationExecutor.executeOperation(openTextFileOperation));
-        System.out.println(textFileOperationExecutor.executeOperation(saveTextFileOperation));
+        TextFileOperation openTextFileOperation =
+                new OpenTextFileOperation(new TextFile("file1.txt"));
+        TextFileOperation saveTextFileOperation =
+                new SaveTextFileOperation(new TextFile("file2.txt"));
+        TextFileOperationExecutor textFileOperationExecutor =
+                new TextFileOperationExecutor();
+
+        System.out.println(
+                textFileOperationExecutor.executeOperation(openTextFileOperation));
+
+        System.out.println(
+                textFileOperationExecutor.executeOperation(saveTextFileOperation));
     }   
 }
